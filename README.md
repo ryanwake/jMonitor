111a3d3882bd0aecb08514d21d4e81604e68db61
Ryan Wake <rwake345@gmail.com>
2016-10-04 15:33:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # jMonitor
 Project for Senior Seminar class.
+
+A monitoring software for macOS made in Java.
+The project utilizes the Oshi library to get different information about the computer. (github.com/oshi)
+Oshi supports macOS, Windows, Linux so the project can easily be extended to support all OS's.
+But in order to keep it within a timeframe for a class project I limited myself to only macOS computers.
+
+The project will consist of two parts, the first one being the data gathering backend that is written in Java.
+The backend will gather the data and send it off to a MySQL database, currently the database is just connected to straight from Java.
+If time allows I will create a RESTful API in order to save the data, but for now that's how it works.
+The second part of the project will be a dashboard for viewing the data that is stored in database.
+My current idea of the dashboard will be written in some form of JavaScript, haven't decided.
---
+++ src/main/java/com/ryanwake/jMonitor/Main.java
@@ -0,0 +1,239 @@
+package com.ryanwake.jMonitor;
+
+
+import org.yaml.snakeyaml.Yaml;
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.NetworkIF;
+import oshi.software.os.OperatingSystem;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Created by ryan on 9/28/16.
+ */
+public class Main {
+    static Map<String, Object> config = null;
+    static Map<String, Object> data = null;
+    static Connection conn = null;
+    static Logger log = Logger.getLogger("jMonitor");
+    static FileHandler fh = null;
+
+    // Use this to export a file from the resources folder to the disk, it will put the file next to the jar file.
+    public static String ExportResource(String resourceName) throws Exception {
+        InputStream stream = null;
+        OutputStream resStreamOut = null;
+        String jarFolder;
+        try {
+            stream = Main.class.getResourceAsStream(resourceName);
+            if (stream == null)
+                throw new Exception("Cannot get resource: " + resourceName + " from Jar file.");
+
+            int readBytes;
+            byte[] buffer = new byte[4096];
+            jarFolder = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath().replace('\\','/');
+            resStreamOut = new FileOutputStream(jarFolder + resourceName);
+            while ((readBytes = stream.read(buffer)) > 0) {
+                resStreamOut.write(buffer, 0, readBytes);
+            }
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            stream.close();
+            resStreamOut.close();
+        }
+        return jarFolder + resourceName;
+    }
+
+    public static void readConfig() {
+        Yaml yaml = new Yaml();
+        Reader reader = null;
+
+        try {
+            reader = new FileReader("./config.yaml");
+            config = (Map<String,Object>) yaml.load(reader);
+        } catch (Exception ex) {
+            System.out.println("Couldn't load config file: " + ex.getLocalizedMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException ioe) {
+                    log.severe("Exception caught while cleaning up config file reader: " + ioe);
+                }
+            }
+        }
+    }
+
+    public static void getData() {
+        SystemInfo si = new SystemInfo();
+        HardwareAbstractionLayer hal = si.getHardware();
+        OperatingSystem os = si.getOperatingSystem();
+
+        data = new HashMap<String, Object>();
+
+        InetAddress ip = null;
+        byte[] mac = null;
+
+        try {
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            mac = network.getHardwareAddress();
+        } catch (Exception ex) {
+            log.warning("Could not get IP or Mac address: " + ex.getLocalizedMessage());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mac.length; i++) {
+            sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+        }
+
+        data.put("mac", sb.toString());
+        data.put("osname", os.getVersion().getCodeName());
+        data.put("osvers", os.getVersion().getVersion());
+        data.put("osbuild", os.getVersion().getBuildNumber());
+        data.put("procFam", si.getHardware().getProcessor().getName());
+        data.put("memory", (int)((si.getHardware().getMemory().getTotal())/1000000000));
+        data.put("ip", ip.getHostAddress());
+
+        for (Map.Entry<String, Object> entry : data.entrySet())
+        {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+    }
+
+    public static void insertData() {
+        String checkQuery = "select * from computers where macAddr = ?";
+        ResultSet rs;
+        try {
+            PreparedStatement checkPrep = conn.prepareStatement(checkQuery);
+            checkPrep.setString(1, data.get("mac").toString());
+            rs = checkPrep.executeQuery();
+            while (rs.next()) {
+                if (rs.next() > 0) {
+                    String query = "insert into computers (macAddr, osname, osvers, osbuild, processor, memory, ip)"
+                            + " values (?, ?, ?, ?, ?, ?, ?)";
+                    PreparedStatement prep = conn.prepareStatement(query);
+                    prep.setString(1, data.get("mac").toString());
+                    prep.setString(2, data.get("osname").toString());
+                    prep.setString(3, data.get("osvers").toString());
+                    prep.setString(4, data.get("osbuild").toString());
+                    prep.setString(5, data.get("procFam").toString());
+                    prep.setInt(6, (int) data.get("memory"));
+                    prep.setString(7, data.get("ip").toString());
+                    prep.execute();
+                } else {
+                    String query = "update computers set osname= ? , osvers = ? , osbuild = ? , processor = ? ,"
+                            + " memory = ? , ip = ? where macAddr = ?";
+                    PreparedStatement prep = conn.prepareStatement(query);
+
+                    prep.setString(1, data.get("osname").toString());
+                    prep.setString(2, data.get("osvers").toString());
+                    prep.setString(3, data.get("osbuild").toString());
+                    prep.setString(4, data.get("procFam").toString());
+                    prep.setInt(5, (int) data.get("memory"));
+                    prep.setString(6, data.get("ip").toString());
+                    prep.setString(7, data.get("mac").toString());
+                }
+
+            }
+
+        } catch (Exception ex) {
+            log.severe("Could not insert data to table: " + ex.getLocalizedMessage());
+        }
+    }
+
+    public static void main (String[] args) {
+        // Time how long it takes to run
+        long startTime = System.nanoTime();
+
+        // Setup logging
+        try {
+            File file = new File("./jMonitor.log");
+            if (file.exists()) {
+                Date date = new Date();
+                SimpleDateFormat df = new SimpleDateFormat("yyyy.MM.dd_HH.mm.ss");
+                File newName = new File("./jMonitor.log_"+ df.format(date));
+                file.renameTo(newName);
+            }
+
+            fh = new FileHandler("./jMonitor.log");
+            log.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            log.info("jMonitor logging started.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        File file = new File("./config.yaml");
+
+        // Load the config file in if it exists, else create default and load defaults.
+        try {
+
+            String fullPath;
+            if (!file.exists()) {
+                fullPath = ExportResource("/config.yaml");
+                log.info("No previous config.yaml found, creating at: " + fullPath);
+                readConfig();
+            } else {
+                log.info("Reading config file...");
+                readConfig();
+            }
+        } catch (Exception ex) {
+            log.severe("Could not load config file! Error message: " + ex.getLocalizedMessage());
+        }
+
+        if ((int)config.get("version") == 0) {
+            try {
+                ExportResource("/config.yaml");
+                log.info("Detected version = 0 in config.yaml, overwriting config.yaml.");
+            } catch (Exception ex) {
+
+            }
+        }
+        log.info("Found config of version: " + config.get("version") + " Located at: " + file.getAbsoluteFile());
+
+        try {
+            conn = DriverManager.getConnection(config.get("url").toString());
+            log.info("Connected to MySQL database.");
+            // Do something with the Connection
+            // Get data about computer hardware
+            getData();
+
+            // Insert the data to MySQL
+            insertData();
+        } catch (SQLException ex) {
+            // handle any errors
+            log.severe("Could not connect to MySQL! Error message: " + ex.getMessage() + " - SQLState: " + ex.getSQLState()
+                    + " - SQLErrorCode: " + ex.getErrorCode());
+        }
+
+
+
+        try {
+            if (!conn.isClosed())
+                conn.close();
+        }
+        catch (SQLException ex) {
+            log.warning("Couldn't close SQL connection. Error message: " + ex.getLocalizedMessage());
+        }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        log.info("Finished execution in " + duration + " milliseconds.");
+    }
+}
